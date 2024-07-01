55dc2d9c5bef9ceaf553c83aada71bef6208159f
Adam Reis <adam@reis.nz>
2024-07-01 14:09:06 +1200

    Remove deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,7 @@
-import js from '@eslint/js'
-import vue from 'eslint-plugin-vue'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 const cfg = await withNuxt(
-  js.configs.recommended,
-  ...vue.configs['flat/recommended'],
+
   {
     languageOptions: {
       ecmaVersion: 'latest',
